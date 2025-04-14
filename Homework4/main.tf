2106520a5eaf1d87ab429dcac009cf68d111831c
akimiyaersoz <kimiyaersoz@icloud.com>
2025-04-14 06:49:44 +0000

    added homework4 files

---
+++ Homework4/main.tf
@@ -0,0 +1,60 @@
+resource "aws_vpc" "main" {
+cidr_block = var.vpc_config.cidr_block
+enable_dns_support = var.vpc_config.enable_dns_support
+enable_dns_hostnames = var.vpc_config.enable_dns_hostnames
+tags = var.tags
+}
+
+resource "aws_subnet" "subnets" {
+count = length(var.subnets)
+vpc_id = aws_vpc.main.id
+cidr_block = var.subnets[count.index].cidr_block
+availability_zone = var.subnets[count.index].availability_zone
+map_public_ip_on_launch = var.subnets[count.index].auto_assign_ip
+tags = merge(var.tags, {
+Name = var.subnets[count.index].name
+})
+}
+
+resource "aws_internet_gateway" "igw" {
+vpc_id = aws_vpc.main.id
+
+tags = merge(var.tags, {
+Name = var.internet_gateway_name
+})
+}
+
+resource "aws_security_group" "main" {
+name = "kaizen-sg"
+description = "Allow ports"
+vpc_id = aws_vpc.main.id
+
+dynamic "ingress" {
+for_each = var.allowed_ports
+content {
+from_port = ingress.value
+to_port = ingress.value
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+}
+
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+tags = var.tags
+}
+
+
+resource "aws_instance" "aws" {
+ami = var.ec2_config["ami_id"]
+instance_type = var.ec2_config["instance_type"]
+subnet_id = aws_subnet.subnets[0].id
+key_name = "my-laptop-key"
+vpc_security_group_ids = [aws_security_group.main.id]
+tags = var.tags
+}
---
+++ Homework4/variables.tf
@@ -0,0 +1,40 @@
+variable "tags" {
+  type    = map(string)
+}
+
+variable "vpc_config" {
+type = object({
+    cidr_block  = string
+    enable_dns_support = bool
+    enable_dns_hostnames = bool
+})
+}
+
+variable "subnets" {
+type = list(object({
+    cidr_block = string
+    availability_zone = string
+    auto_assign_ip = bool
+    name = string
+}) ) 
+}
+
+variable "internet_gateway_name" {
+type = string  
+}
+
+variable "route_table_names" {
+type =list(string)
+}
+
+variable "allowed_ports" {
+type    = list(number)
+}
+
+variable "ec2_config" {
+type = map(string)
+}
+
+variable "key_name" {
+  type = string
+}
